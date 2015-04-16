98c5c64f42bcada814298c6e44628fc2ad472231
Philip van Oosten <philip@vanoosten.be>
2015-04-17 00:06:11 +0200

    Creating WPF browse application for Neo4J

---
+++ NeoBrowser/GraphBrowser.cs
@@ -0,0 +1,46 @@
+﻿using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoBrowser
+{
+    public class GraphBrowser
+    {
+        private GraphClient _client;
+
+        public GraphBrowser(string NeoUrl, string NeoUser, string NeoPassword)
+        {
+            // TODO: Complete member initialization
+            Url = NeoUrl;
+            User = NeoUser;
+            Password = NeoPassword;
+            Connect();
+        }
+
+        public string Url { get; private set; }
+        public string User { get; private set; }
+        public string Password { get; private set; }
+
+
+        private void Connect()
+        {
+            _client = new GraphClient(new Uri(Url));
+            _client.Connect();
+        }
+
+
+        void browse()
+        {
+            var res = _client.Cypher.Match("", "").Return<object>("").Results;
+            
+        }
+
+        public void Close()
+        {
+            // close connection to graph database
+        }
+    }
+}
---
+++ NeoBrowser/ViewModels/MainWindow_ViewModel.cs
@@ -0,0 +1,81 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NeoBrowser.ViewModels
+{
+    public class MainWindow_ViewModel: ViewModelBase
+    {
+        private string _neoUrl;
+        private string _neoUser;
+        private string _neoPassword;
+
+        public MainWindow_ViewModel()
+        {
+            NeoUrl = "http://localhost:7474";
+            NeoUser = "Neo4j";
+            NeoPassword = "Neo4j";
+            ConnectCommand = new RelayCommand(Connect);
+        }
+
+        public ICommand ConnectCommand{get; private set;}
+
+        private void Connect()
+        {
+            dynamic oldBrowser = App.Current.TryFindResource("browser");
+            if (oldBrowser != null)
+            {
+                oldBrowser.Close();
+            }
+            App.Current.Resources["browser"] = new GraphBrowser(NeoUrl, NeoUser, NeoPassword);
+        }
+
+        public string NeoUrl
+        {
+            get
+            {
+                return _neoUrl;
+            }
+            set
+            {
+                if (_neoUrl == value) return;
+                _neoUrl = value;
+                RaisePropertyChanged("NeoUrl");
+            }
+        }
+        public string NeoUser
+        {
+            get
+            {
+                return _neoUser;
+            }
+            set
+            {
+                if (_neoUser == value) return;
+                _neoUser = value;
+                RaisePropertyChanged("NeoUser");
+            }
+        }
+        public string NeoPassword
+        {
+            get
+            {
+                return _neoPassword;
+            }
+            set
+            {
+                if (_neoPassword == value) return;
+                _neoPassword = value;
+                RaisePropertyChanged("NeoPassword");
+            }
+        }
+
+
+
+    }
+}
