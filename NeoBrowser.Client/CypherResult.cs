d5643af5cc9bd1af3b6a933bf1e1f96a59bac94b
Philip van Oosten <philip@vanoosten.be>
2015-05-09 00:07:47 +0200

    Implemented sending cypher query without transaction

--- NeoBrowser.Client/CypherResult.cs
+++ NeoBrowser.Client/CypherResult.cs
@@ -5,7 +5,21 @@ using System.Text;
 
 namespace NeoBrowser.Client
 {
+    /// <summary>
+    /// The result of a Cypher query.
+    /// </summary>
     public class CypherResult
     {
+        internal CypherResult(CypherStatement statement, string[] columns, IEnumerable<CypherResultRow> data)
+        {
+            Statement = statement;
+            Columns = columns;
+            Data = data;
+        }
+
+
+        public CypherStatement Statement { get; private set; }
+        public string[] Columns { get; private set; }
+        public IEnumerable<CypherResultRow> Data { get; private set; }
     }
 }
---
+++ NeoBrowser.Client/CypherResultRow.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NeoBrowser.Client
+{
+    public class CypherResultRow
+    {
+        private readonly JToken _rowData;
+
+        public CypherResultRow(JToken rowData)
+        {
+            _rowData = rowData;
+        }
+
+        public JToken this[int index]
+        {
+            get
+            {
+                return _rowData[index];
+            }
+        }
+
+        public T Get<T>(int index)
+        {
+            return _rowData[index].Value<T>();
+        }
+    }
+}
